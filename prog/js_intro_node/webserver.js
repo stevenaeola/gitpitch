d481ca2d50d4cfd146515f1bb7de2756d5739e89
dcs0spb <s.p.bradley@durham.ac.uk>
2019-02-06 10:15:55 +0000

    example node scrips

--- prog/js_intro_node/webserver.js
+++ prog/js_intro_node/webserver.js
@@ -1,8 +1,9 @@
 http = require("http")
 
 http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+    console.log("request received");
 }).listen(8080);
 
 console.log('Server running at http://127.0.0.1:8080/');
