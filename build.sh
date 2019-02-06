fe35d841fe48d14240e525d71a90149076aeb2f1
dcs0spb <s.p.bradley@durham.ac.uk>
2019-02-06 10:13:05 +0000

    build uses presentation name

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sed '/^---/ d' PITCHME.md | sed '/^@/ d' > README.md
+PATHEND=$(pwd | rev | cut -d'/' -f-2 | rev)
 git add .
-git commit -m "Working on presentation"
+git commit -m "Working on presentation $PATHEND"
 git push -u origin master
-echo "https://gitpitch.com/stevenaeola/gitpitch/master?p=prog/git"
+echo "https://gitpitch.com/stevenaeola/gitpitch/master?p=${PATHEND}"
